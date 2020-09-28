0f62e767a8eb8a84c34644a4373a2f610e1926aa
nah1222 <nah1222@yahoo.com>
2020-09-28 16:34:55 -0400

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+50.times do 
+    User.create(name: Faker::Artist.name, email: Faker::Internet.email, password: "abc123")
+end
+
+30.times do
+    Engineer.create(name: Faker::DcComics.name, bio: Faker::ChuckNorris.fact)
+end
+
+10.times do 
+    Studio.create(name: Faker::Company.name, location: Faker::Address.full_address, info: Faker::ChuckNorris.fact)
+end
+
+70.times do
+    Schedule.create(user_id: User.all.sample.id, studio_id: Studio.all.sample.id, engineer_id: Engineer.all.sample.id)
+end
