941bbe02f51e737ae8b0cb10c8ebf82113b04bd9
D1yr <alexander.m.sahli@gmail.com>
2020-10-02 08:49:27 -0400

    started adding assets

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,6 +22,6 @@
 #    Studio.create(name: Faker::Company.name, location: Faker::Address.full_address, info: Faker::ChuckNorris.fact)
 #end
 
-70.times do
+#70.times do
    # Schedule.create(user_id: User.all.sample.id, studio_id: Studio.all.sample.id, engineer_id: Engineer.all.sample.id, time: rand(1..10), date: Faker::Date.between(from: '2020-09-30', to: '2021-09-25'))
 #end
